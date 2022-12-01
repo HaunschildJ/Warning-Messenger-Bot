8b59712ae72a0cde84dfcd775a4f1d0afd62618d
Lauren <lauren.pflueger@gmail.com>
2022-12-01 16:36:50 +0100

    Corona Command handling without nina response

--- Source/ChatSender.py
+++ Source/ChatSender.py
@@ -5,3 +5,6 @@ bot = Bot.bot
 def send_message(chat_id, message_string):
     bot.send_message(chat_id, message_string)
 
+
+def send_chat_action(chat_id, action):
+    bot.send_chat_action(chat_id, action)
--- Source/TextTemplates.py
+++ Source/TextTemplates.py
@@ -3,3 +3,13 @@ import string
 
 def get_greeting_string() -> string:
     return "Guten Tag"
+
+
+def get_corona_string() -> string:
+    return """
+Corona Inzidenz: %inzidenz 
+Aktuelle Fallzahlen %case 
+Aktuelle Todeszahlen %death 
+Informationen vom %date """
+
+
