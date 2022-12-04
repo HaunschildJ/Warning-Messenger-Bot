5149bbed3c278f79c6168619733ed18a4880c2eb
Ethem <73109035+EthemGit@users.noreply.github.com>
2022-12-04 13:47:52 +0100

    python Konventionen geändert

--- Source/Controller.py
+++ Source/Controller.py
@@ -1,5 +1,4 @@
 import ChatSender
 import TextTemplates
-
 def start(chat_id):
     ChatSender.send_message(chat_id, TextTemplates.get_greeting_string())
--- Source/NinaStringHelper.py
+++ Source/NinaStringHelper.py
@@ -1,24 +1,24 @@
 
 #TODO mit was werden die Tags ersetzt? Leerzeichen?, Absatz? etc...
 def filter_html_tags(s : str) -> str:
-    filteredString = ""
-    openedBracketsCounter = 0
+    filtered_string = ""
+    opened_brackets_counter = 0
 
     for i in range(0, len(s)):
          c = s[i]
 
-         if (c == '<'):
-             openedBracketsCounter += 1
+         if c == '<':
+             opened_brackets_counter += 1
 
-         if (openedBracketsCounter == 0):
-             filteredString += c
+         if opened_brackets_counter == 0:
+             filtered_string += c
 
-         if (c == '>'):
-             openedBracketsCounter -= 1
-             if (openedBracketsCounter == 0 and i+1 < len(s) and  s[i+1] != '<'):
-                filteredString += ' '
+         if c == '>':
+             opened_brackets_counter -= 1
+             if opened_brackets_counter == 0 and i+1 < len(s) and  s[i + 1] != '<':
+                filtered_string += ' '
 
-    return  filteredString
+    return  filtered_string
 
 #fill till 12 places with zeros
 def expand_location_id_with_zeros(location_id : str):
