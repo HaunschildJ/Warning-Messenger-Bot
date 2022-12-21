2ebc86909f5a91f036ee318252afec6f0c726970
GlebLava <60477842+GlebLava@users.noreply.github.com>
2022-12-21 15:42:15 +0100

    get_detailed_warningn zu NinaService hinzugefügt. Wird einer der warnings gepollt, so bekommt man eine warning id. Mit dieser warning id kann man das get_detailed_warning requesten

--- Source/NinaService.py
+++ Source/NinaService.py
@@ -1,9 +1,9 @@
-from dataclasses import  dataclass
+from dataclasses import dataclass
 from enum import Enum
+from datetime import datetime
 import requests
 import NinaStringHelper
 import NinaPlaces
-
 
 baseUrl = "https://warnung.bund.de/api31"
 
@@ -19,6 +19,7 @@ class CovidRules:
     hospital_rules: str
     travelling_rules: str
     fines: str
+
 
 def get_covid_rules(city_name) -> CovidRules:
     city_code = NinaPlaces.get_kreis_id(city_name)
@@ -40,6 +41,7 @@ def get_covid_rules(city_name) -> CovidRules:
 
     return CovidRules(vaccine_info, contact_terms, school_kita_rules, hospital_rules, travelling_rules, fines)
 
+
 @dataclass
 class CovidInfos:
     infektion_danger_level: str
@@ -49,7 +51,7 @@ class CovidInfos:
 
 
 def get_covid_infos(city_name) -> CovidInfos:
-    city_code =  NinaPlaces.get_kreis_id(city_name)
+    city_code = NinaPlaces.get_kreis_id(city_name)
     # der city_code muss 12 Stellig sein, was fehlt muss mit 0en aufgefüllt werden laut doku
     # https://github.com/bundesAPI/nina-api/blob/main/Beispielcode/Python/CoronaZahlenNachGebietscode.py
     city_code = NinaStringHelper.expand_location_id_with_zeros(city_code)
@@ -69,17 +71,32 @@ def get_covid_infos(city_name) -> CovidInfos:
     return CovidInfos(infektion_danger_level, sieben_tage_inzidenz_kreis, sieben_tage_inzidenz_bundesland, general_tips)
 
 
-
 class WarningSeverity(Enum):
     Minor = 0
     Moderate = 1
     Severe = 2
     Unkown = 3
 
+
+def _get_warning_severity(severity: str) -> WarningSeverity:
+    try:
+        return WarningSeverity[severity]
+    except KeyError:
+        return WarningSeverity.Unkown
+
+
 class WarningType(Enum):
     Update = 0
     Alert = 1
     Unkown = 2
+
+
+def _get_warning_type(type: str) -> WarningType:
+    try:
+        return WarningType[type]
+    except KeyError:
+        return WarningType.Unkown
+
 
 @dataclass
 class GeneralWarning:
@@ -91,11 +108,17 @@ class GeneralWarning:
     title: str
 
 
-def poll_general_warning(api_string : str) -> list[GeneralWarning]:
+def _translate_time(nina_time: str) -> str:
+    dt = datetime.fromisoformat(nina_time)
+
+    # Convert the datetime object to a string in a specific format
+    normal_time_string = dt.strftime("%Y-%m-%d %I:%M")
+    return normal_time_string
+
+
+def _poll_general_warning(api_string: str) -> list[GeneralWarning]:
     response_raw = requests.get(baseUrl + api_string)
     response = response_raw.json()
-
-    print(response_raw.text)
 
     warning_list = []
 
@@ -105,56 +128,49 @@ def poll_general_warning(api_string : str) -> list[GeneralWarning]:
     for i in range(0, len(list(response))):
         id = response[i]["id"]
         version = response[i]["version"]
-        start_date = response[i]["startDate"]
 
-        try:
-            severity = WarningSeverity[response[i]["severity"]]
-        except KeyError:
-            severity = WarningSeverity.Unkown
+        start_date = _translate_time(response[i]["startDate"])
 
-        try:
-            type = WarningType[response[i]["type"]]
-        except KeyError:
-            type = WarningType.Unkown
-
+        severity = _get_warning_severity(response[i]["severity"])
+        type = _get_warning_type(response[i]["type"])
         title = response[i]["i18nTitle"]["de"]
-        warning_list.append(GeneralWarning(id = id, version= version, start_date= start_date, severity=severity, type= type, title= title))
+        warning_list.append(
+            GeneralWarning(id=id, version=version, start_date=start_date, severity=severity, type=type, title=title))
 
     return warning_list
 
 
 def poll_biwapp_warning() -> list[GeneralWarning]:
     biwapp_API = "/biwapp/mapData.json"
-    return poll_general_warning(biwapp_API)
+    return _poll_general_warning(biwapp_API)
 
 
 def poll_katwarn_warning() -> list[GeneralWarning]:
     katwarn_API = "/katwarn/mapData.json"
-    return poll_general_warning(katwarn_API)
+    return _poll_general_warning(katwarn_API)
 
 
 def poll_mowas_warning() -> list[GeneralWarning]:
     mowas_API = "/mowas/mapData.json"
-    return poll_general_warning(mowas_API)
+    return _poll_general_warning(mowas_API)
 
 
 def poll_dwd_warning() -> list[GeneralWarning]:
     dwd_API = "/dwd/mapData.json"
-    return poll_general_warning(dwd_API)
+    return _poll_general_warning(dwd_API)
 
 
 def poll_lhp_warning() -> list[GeneralWarning]:
     lhp_API = "/lhp/mapData.json"
-    return poll_general_warning(lhp_API)
+    return _poll_general_warning(lhp_API)
 
 
 def poll_police_warning() -> list[GeneralWarning]:
     police_API = "/police/mapData.json"
-    return poll_general_warning(police_API)
-
+    return _poll_general_warning(police_API)
 
 """
-warningList = poll_biwapp_warning() #for testing you just need to change which warning method you call here
+warningList = poll_biwapp_warning()  # for testing you just need to change which warning method you call here
 for warning in warningList:
     print("\n")
     print(warning.id)
@@ -162,13 +178,118 @@ for warning in warningList:
     print(warning.severity)
     print(warning.type)
     print(warning.title)
+    print(warning.start_date)
 """
 
+@dataclass
+class DetailedWarningInfoArea:
+    area_description: str
+    geocode: list[str]
+
+
+@dataclass
+class DetailedWarningInfo:
+    event: str  # noch keine Ahnung was das sein soll
+    severity: WarningSeverity
+    date_expires: str
+    headline: str
+    description: str
+    area: list[DetailedWarningInfoArea]
+
+
+@dataclass
+class DetailedWarning:
+    id: str
+    sender: str
+    date_sent: str
+    status: str
+    infos: list[DetailedWarningInfo]
+
+
+def _get_detailed_warning_infos_area_geocode(response_geocode) -> list[str]:
+    geocode = []
+    if response_geocode is None:
+        return geocode
+
+    for i in range(0, len(response_geocode)):
+        geocode.append(response_geocode[i]["value"])
+
+    return  geocode
+
+def _get_detailed_warning_infos_area(response_area) -> list[DetailedWarningInfoArea]:
+    area = []
+    if response_area is None:
+        return area
+
+    for i in range(0, len(response_area)):
+        area_description = response_area[i]["areaDesc"]
+        geocode = _get_detailed_warning_infos_area_geocode(response_area[i]["geocode"])
+        area.append(
+            DetailedWarningInfoArea(area_description=area_description, geocode=geocode)
+        )
+
+    return  area
+
+def _get_detailed_warning_infos(response_infos) -> list[DetailedWarningInfo]:
+    infos = []
+    if response_infos is None:
+        return  infos
+
+    for i in range(0, len(response_infos)):
+        event = response_infos[i]["event"]
+        severity = _get_warning_severity(response_infos[i]["severity"])
+        date_expires = _translate_time(response_infos[i]["expires"])
+        headline = response_infos[i]["headline"]
+        description = NinaStringHelper.filter_html_tags(response_infos[i]["description"])
+        area = _get_detailed_warning_infos_area(response_infos[i]["area"])
+
+        infos.append(
+            DetailedWarningInfo(event=event, severity=severity, date_expires=date_expires, headline=headline,
+                                description=description, area=area)
+        )
+
+    return  infos
+
+def get_detailed_warning(warning_id: str):
+    """
+    This method should be called after a warning with one of the poll_****_warning methods was received
+    :param warning_id: warning id is extracted from the poll_****_warning method return type: GeneralWarning.id
+    :return:
+    """
+    response_raw = requests.get(baseUrl + "/warnings/" + warning_id + ".json")
+    response = response_raw.json()
+    print(response_raw.text)
+
+    id = response["identifier"]
+    sender = response["sender"]
+    date_sent = _translate_time(response["sent"])
+    status = response["status"]
+    infos = _get_detailed_warning_infos(response["info"])
+
+    return DetailedWarning(id=id, sender=sender, date_sent=date_sent, status=status, infos=infos)
 
 
 
 
 
-
-
-
+"""
+warning = poll_biwapp_warning()[0] #for testing the individual warning method needs to return a warning (not always the case, just iterate through biwap, katwarn, police, etc...)
+warning = get_detailed_warning(warning.id)
+print(warning.id)
+print(warning.status)
+print(warning.sender)
+print(warning.date_sent)
+print("INFOS: ")
+for i in range(0, len(warning.infos)):
+    print("\t"+warning.infos[i].event)
+    print("\t"+warning.infos[i].severity.name)
+    print("\t"+warning.infos[i].date_expires)
+    print("\t"+warning.infos[i].headline)
+    print("\t"+warning.infos[i].description)
+    print("\tAREA: ")
+    for j in range(0, len(warning.infos[i].area)):
+        print("\t\t"+ warning.infos[i].area[i].area_description)
+        for l in range(0, len(warning.infos[i].area[j].geocode)):
+            print ("\t\tGEOCODE:")
+            print("\t\t\t"+ warning.infos[i].area[i].geocode[l])
+"""
