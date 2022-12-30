5ce96725a948ca98fb499a4e0dd4498748992f82
Ethem <73109035+EthemGit@users.noreply.github.com>
2022-12-30 15:11:35 +0100

    added Test Results and 2 .idea files to .gitignore, 2) 100% coverage for nina_string_helper_test including new comments to clarify code

--- tests/nina_string_helper_test.py
+++ tests/nina_string_helper_test.py
@@ -18,19 +18,34 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(should_be, nina_string_helper.expand_location_id_with_zeros(input_value))
 
     def test_filter_html_tags(self):
+        # normal valid html without edge case
         input_value = "<html><head>firstContent</head><body>-secondContent</body></html>"
         should_be = "firstContent-secondContent"
-
         self.assertEqual(should_be, nina_string_helper.filter_html_tags(input_value))
 
+        # test extract_till_char(..) method with edge case that you start late and don't find stop_char
+        input_value = "testStringWithALotOfCharacters"
+        index = len(input_value) - 3  # start late
+        stop_char = "x"  # char that will not be found
+        should_be = "ers"
+        self.assertEqual(should_be, nina_string_helper.extract_till_char(input_value, index, stop_char))
+
+        # valid html with paragraph ( \n )
+        input_value = "<html><head>firstContent</head><body></p>secondContent</body></html>"
+        should_be = "firstContent\nsecondContent"
+        self.assertEqual(should_be, nina_string_helper.filter_html_tags(input_value))
+
+        # valid html with link and valid hyperlink text
         input_value = "<html><head>first </head><body><a href=\"https://www.w3schools.com/\">visitW3!</a></body></html>"
         should_be = "first visitW3!: https://www.w3schools.com/"
         self.assertEqual(should_be, nina_string_helper.filter_html_tags(input_value))
 
+        # valid html with link and short hyperlink text
         input_value = "<html><head>first </head><body><a href=\"https://www.w3schools.com/\">a</a></body></html>"
         should_be = "first a: https://www.w3schools.com/"
         self.assertEqual(should_be, nina_string_helper.filter_html_tags(input_value))
 
+        # valid html with link and without hyperlink text
         input_value = "<html><head>first </head><body><a href=\"https://www.w3schools.com/\"></a></body></html>"
         should_be = "first "
         self.assertEqual(should_be, nina_string_helper.filter_html_tags(input_value))
