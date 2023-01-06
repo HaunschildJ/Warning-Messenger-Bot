41bd803e9ac1f60588731db97bfddec67e0c21f2
ElifnurD <73185659+ElifnurD@users.noreply.github.com>
2023-01-06 21:12:03 +0100

    DocStrings and Tests

--- tests/place_converter_test.py
+++ tests/place_converter_test.py
@@ -34,22 +34,7 @@ class MyTestCase(unittest.TestCase):
         should_be = ["Oberursel (Taunus)", "06434"]
         self.assertEqual(should_be, place_converter._postal_code_dictionary[input_value])
 
-    def test_get_district_id(self):
-        # is a valid place
-        input_value = "Frankfurt am Main, Stadt"
-        should_be = "06412"
-        self.assertEqual(should_be, place_converter.get_district_id(input_value))
-
-        # is a valid district
-        input_value = "Hochtaunuskreis"
-        should_be = "06434"
-        self.assertEqual(should_be, place_converter.get_district_id(input_value))
-
-        # cannot be found
-        input_value = "no"
-        self.assertRaises(ValueError, place_converter.get_district_id, input_value)
-
-    def test_get_district_id_for_district_name(self):
+    def test_get_dicts_for_exact_district_name(self):  # TODO
         # is a valid district
         input_value = "Hochtaunuskreis"
         should_be = "06434"
@@ -57,66 +42,30 @@ class MyTestCase(unittest.TestCase):
 
         # cannot be found
         input_value = "no"
-        self.assertEqual(None, place_converter.get_district_id_for_district_name(input_value))
-
-    def test_get_district_id_for_place_name(self):
-        # is a valid place
-        input_value = "Frankfurt am Main, Stadt"
-        should_be = "06412"
-        self.assertEqual(should_be, place_converter.get_district_id_for_place_name(input_value))
-
-        # cannot be found
-        input_value = "no"
-        self.assertEqual(None, place_converter.get_district_id_for_place_name(input_value))
+        self.assertEqual([], place_converter.get_district_id_for_district_name(input_value))
 
     def test_get_place_for_postal_code(self):
         # is a valid postal code
         input_value = "61440"
-        should_be = "Oberursel (Taunus)"
+        should_be = "Oberursel (Taunus)", "06434"
         self.assertEqual(should_be, place_converter.get_place_for_postal_code(input_value))
 
         # cannot be found
         input_value = "no"
         self.assertRaises(ValueError, place_converter.get_place_for_postal_code, input_value)
 
-    def test_get_district_for_postal_code(self):
+    def test_get_district_name_for_postal_code(self):
         # is a valid postal code
         input_value = "61440"
         should_be = "Hochtaunuskreis"
-        self.assertEqual(should_be, place_converter.get_district_for_postal_code(input_value))
+        self.assertEqual(should_be, place_converter.get_district_name_for_postal_code(input_value))
 
         # cannot be found
         input_value = "no"
-        self.assertEqual(None, place_converter.get_district_for_postal_code(input_value))
+        self.assertEqual(None, place_converter.get_district_name_for_postal_code(input_value))
 
-    def test_get_similar_names(self):
+    def test_get_dicts_for_exact_place_name(self):
         pass
-
-    def test_get_similar_districts(self):
-        pass
-
-    def test_get_similar_places(self):
-        pass
-
-    def test_get_district_name_for_place(self):
-        # is a valid place name
-        input_value = "Frankfurt am Main, Stadt"
-        should_be = "Frankfurt am Main"
-        self.assertEqual(should_be, place_converter.get_district_name_for_place(input_value))
-
-        # cannot be found
-        input_value = "no"
-        self.assertRaises(ValueError, place_converter.get_district_name_for_place, input_value)
-
-    def test_get_place_id_for_place_name(self):
-        # is a valid place name
-        input_value = "Frankfurt am Main, Stadt"
-        should_be = "064120000000"
-        self.assertEqual(should_be, place_converter.get_place_id_for_place_name(input_value))
-
-        # cannot be found
-        input_value = "no"
-        self.assertRaises(ValueError, place_converter.get_place_id_for_place_name, input_value)
 
     def test_get_name_for_id(self):
         # is a valid place id
@@ -133,25 +82,20 @@ class MyTestCase(unittest.TestCase):
         input_value = "no"
         self.assertRaises(ValueError, place_converter.get_name_for_id, input_value)
 
-    def test_get_place_name(self):
-        # is a valid place id
-        input_value = "064120000000"
-        should_be = "Frankfurt am Main, Stadt"
-        self.assertEqual(should_be, place_converter.get_place_name(input_value))
+    def test_get_suggestions_for_place_name(self):
+        pass
 
-        # cannot be found
-        input_value = "no"
-        self.assertEqual(None, place_converter.get_place_name(input_value))
+    def test_get_place_dict_suggestions(self):
+        pass
 
-    def test_get_district_name(self):
-        # is a valid district id
-        input_value = "06412"
-        should_be = "Frankfurt am Main"
-        self.assertEqual(should_be, place_converter.get_district_name(input_value))
+    def test_get_suggestions_for_district_name(self):
+        pass
 
-        # cannot be found
-        input_value = "no"
-        self.assertEqual(None, place_converter.get_district_name(input_value))
+    def test_get_district_dict_suggestions(self):
+        pass
+
+    def test_get_dict_suggestions(self):
+        pass
 
 
 if __name__ == '__main__':
